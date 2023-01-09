302218c861bac3e1d0aaf8ca6020662149ba8b9b
Rafael Simionato <rafaasimi@hotmail.com>
2023-01-09 13:47:56 -0300

    Material sobre instalação e uso de bibliotecas externas

---
+++ desenvolvimento/bibliotecas_externas/src/script.js
@@ -0,0 +1,6 @@
+var body = $('body');
+body.addClass('ativo');
+_.intersection([1, 2, 3, 5, 6], [2, 3, 1, 9]);
+var iframe = document.getElementById('vimeo');
+var player = new Vimeo.Player(iframe);
+console.log(player);
