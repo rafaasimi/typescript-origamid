598a66b4aa3f46b5378eb678644af7f7a0efa7bc
Rafael Simionato <rafaasimi@hotmail.com>
2023-01-12 10:29:52 -0300

    Criado função de show/hide para exibir/ocultar os slides

--- pratica_dom/dist/Slide.js
+++ pratica_dom/dist/Slide.js
@@ -1,17 +1,27 @@
 export class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slide;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
-        console.log(container);
-        console.log(elements);
-        console.log(controls);
-        console.log(time);
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.show(this.index);
+    }
+    hide(el) {
+        el.classList.remove('active');
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((el) => this.hide(el));
+        this.slide.classList.add('active');
     }
 }
 //# sourceMappingURL=Slide.js.map
