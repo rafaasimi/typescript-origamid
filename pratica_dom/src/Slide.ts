07329a8d0f064933c8fe98e241e5fb9994b8c4d9
Rafael Simionato <rafaasimi@hotmail.com>
2023-01-12 10:43:55 -0300

    Criado funções (prev/next) para controlar os slides

--- pratica_dom/src/Slide.ts
+++ pratica_dom/src/Slide.ts
@@ -20,7 +20,7 @@ export class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
 
   hide(el: Element) {
@@ -32,5 +32,33 @@ export class Slide {
     this.slide = this.slides[this.index]
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add('active');
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev)
+  }
+
+  next() {
+    const next = (this.index + 1) < this.slides.length ? this.index + 1 : 0
+    this.show(next)
+  }
+
+  private addControls() {
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    prevButton.innerText = 'Slide anterior'
+    nextButton.innerText = 'Próximo slide'
+
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+    
+    prevButton.addEventListener('pointerup', () => this.prev())
+    nextButton.addEventListener('pointerup', () => this.next())
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
